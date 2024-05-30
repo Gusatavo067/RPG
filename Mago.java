3164818f9b8c8736c12d679a8736abc0c0d3f42b
Gustavoh <146787760+Gusatavo067@users.noreply.github.com>
2024-05-30 19:05:08 -0400

    adicao de file e alteracao de codigo

--- Mago.java
+++ Mago.java
@@ -1,5 +1,5 @@
 class Mago extends Personagem{
-    public Mago(double saude, double forca, double destreza){
-        super("Mago", saude, forca, destreza);
+    public Mago(double saude, double forca, double destreza, Arma arma){
+        super("Mago", saude, forca, destreza, arma);
     }
 } 
